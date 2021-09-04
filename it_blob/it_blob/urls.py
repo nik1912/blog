1086fb586ee0c3b80ee412adb9504453d6e504ab
nik1912 <nikolajsolovej10@gmail.com>
2021-09-04 10:21:03 +0300

    home page

--- it_blob/it_blob/urls.py
+++ it_blob/it_blob/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.http import request
-from django.urls import path
-from django.urls.conf import include, include
-from django.shortcuts import redirect 
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('', lambda request: redirect('/home/'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
