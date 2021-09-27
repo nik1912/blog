5cef72f98b68a6134bfbbd00e5bf66343a5bca74
nik1912 <nikolajsolovej10@gmail.com>
2021-09-27 20:53:48 +0300

    login and validation form

--- it_blob/user/urls.py
+++ it_blob/user/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from user.views import signin
+from user.views import Registration, Login, logout
 from django.shortcuts import redirect
 
 
 
-
 urlpatterns = [
-    path("signin/", signin, name="signin_page"),
-    # path("login/", login, name="login_page"),
-    # path("", lambda request: redirect("/user/login/")),
+    path("signin/", Registration.as_view(), name="signin_page"),
+    path("login/", Login.as_view(), name="login_page"),
+    path("logout/", logout, name="logout_page"),
+    path("", lambda request: redirect("/user/login/")),
 ]
